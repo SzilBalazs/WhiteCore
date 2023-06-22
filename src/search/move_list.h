e7e3f9cd2bf189cc2bf48ca3ee933e6989f9e86f
SzilBalazs <szilprogramming@gmail.com>
2023-06-22 17:41:31 +0200

    Move ordering Bench: 18392893

--- src/search/move_list.h
+++ src/search/move_list.h
@@ -24,19 +24,33 @@ class MoveList {
 
 public:
 
-	MoveList(const Board &board) {
-		moves_end = Movegen::gen_moves(board, moves, captures_only);
-		it = moves;
+	MoveList(const Board &board) : current(0) {
+		size = Movegen::gen_moves(board, moves, captures_only) - moves;
+		for (unsigned int i = 0; i < size; i++) {
+			scores[i] = score_move(moves[i]);
+		}
 	}
 
 	bool empty() {
-		return moves_end == it;
+		return current == size;
 	}
 
 	Move next_move() {
-		return *it++;
+		for (unsigned int i = current; i < size; i++) {
+			if (scores[i] > scores[current]) {
+				std::swap(scores[i], scores[current]);
+				std::swap(moves[i], moves[current]);
+			}
+		}
+		return moves[current++];
 	}
 
 private:
-	Move moves[200], *it, *moves_end;
+	Move moves[200];
+	unsigned int size, current;
+	Score scores[200];
+
+	Score score_move(Move move) {
+		return 0;
+	}
 };
