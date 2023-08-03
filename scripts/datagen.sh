51efb1882f64eadfb0e4ecad01c7a365f57d952d
SzilBalazs <szilprogramming@gmail.com>
2023-08-03 15:43:42 +0200

    Add example usage to datagen.sh
    Bench: 3091908

--- scripts/datagen.sh
+++ scripts/datagen.sh
@@ -1,5 +1,11 @@
 #!/bin/bash
 
+if [ $# -eq 0 ]
+  then
+    echo "Example usage: ./datagen.sh \"gen nodes 1000 threads 4 games 1000\""
+    exit 1
+fi
+
 git clone https://github.com/SzilBalazs/WhiteCore
 make EXE=Binary -C WhiteCore
 cp WhiteCore/Binary Binary
