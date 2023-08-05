91add340baeb8276987fa09640345af7bf96b4f1
SzilBalazs <szilprogramming@gmail.com>
2023-08-05 19:24:57 +0200

    SE 2
    Bench: 1664350

--- src/search/search_thread.h
+++ src/search/search_thread.h
@@ -300,7 +300,7 @@ namespace search {
                 if (in_check) {
                     extensions = 1;
                 } else if (non_root_node && depth >= 7 && move == hash_move &&
-                           entry->flag == TT_BETA && entry->depth >= depth - 3 &&
+                           entry->flag == TT_BETA && entry->depth >= depth &&
                            std::abs(entry->eval) < WORST_MATE) {
 
                     Score singular_beta = entry->eval - depth * 3;
