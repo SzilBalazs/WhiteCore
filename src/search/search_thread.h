0c8721fc10562ff612a3d6ccb4ba36e542072e71
SzilBalazs <szilprogramming@gmail.com>
2023-08-26 19:41:16 +0200

    Tweak LMR history extensions
    Bench: 12530622

--- src/search/search_thread.h
+++ src/search/search_thread.h
@@ -406,7 +406,7 @@ namespace search {
 
                     R -= pv_node;
                     R += !improving;
-                    R -= std::clamp(history.butterfly[move.get_from()][move.get_to()] / 8192, -2, 2);
+                    R -= std::clamp(history.butterfly[move.get_from()][move.get_to()] / 4096, -2, 2);
 
                     Depth D = std::clamp(new_depth - R, 1, depth - 1);
                     score = -search<NON_PV_NODE>(D, -alpha - 1, -alpha, ss + 1);
