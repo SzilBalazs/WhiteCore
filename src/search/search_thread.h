494cfc78d11d7e474e61746add6f526641fa7e95
SzilBalazs <szilprogramming@gmail.com>
2023-08-31 17:29:22 +0200

    Take 4
    Bench: 9802458

--- src/search/search_thread.h
+++ src/search/search_thread.h
@@ -407,7 +407,7 @@ namespace search {
 
                     R -= pv_node;
                     R += !improving;
-                    R -= history.cutoffs[ss->ply + 1] <= 5;
+                    R += (move == history.killer_moves[ss->ply][0] || move == history.killer_moves[ss->ply][1]) && history.cutoffs[ss->ply] > 4;
                     R -= std::clamp(history.butterfly[move.get_from()][move.get_to()] / 4096, -2, 2);
 
                     Depth D = std::clamp(new_depth - R, 1, depth - 1);
