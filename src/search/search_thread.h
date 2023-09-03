18cafdec2da064b84d566d0ba59988306b78b122
SzilBalazs <szilprogramming@gmail.com>
2023-09-03 14:02:40 +0200

    Allow LMR extensions
    Bench: 9833730

--- src/search/search_thread.h
+++ src/search/search_thread.h
@@ -402,7 +402,7 @@ namespace search {
                     R += !improving;
                     R -= std::clamp(history.get_history(move, ss) / 4096, -2, 2);
 
-                    Depth D = std::clamp(new_depth - R, 1, depth - 1);
+                    Depth D = std::clamp(new_depth - R, 1, int(depth));
                     score = -search<NON_PV_NODE>(D, -alpha - 1, -alpha, ss + 1);
 
                     if (score > alpha && R > 1) {
